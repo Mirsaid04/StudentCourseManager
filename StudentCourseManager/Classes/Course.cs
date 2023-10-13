d13018a6d8194c9ef3cc6ea78342af3b8c779af6
Mirsaid04 <mirsaidd04@gmail.com>
2023-10-13 10:00:43 +0900

    Add Course Class File

---
+++ StudentCourseManager/Classes/Course.cs
@@ -0,0 +1,16 @@
+﻿namespace StudentCourseManager.Classes
+{
+    internal class Course
+    {
+        public string CourseName { get; set; }
+        public string CourseCode { get; set; }
+        public int Credits { get; set; }
+
+        public Course(string courseName, string courseCode, int credits)
+        {
+            CourseName = courseName;
+            CourseCode = courseCode;
+            Credits = credits;
+        }
+    }
+}
